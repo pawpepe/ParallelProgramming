da885a28ac9b6c60c1258fabbd46b6e83940ff50
Anna <apawlickamaule@gmail.com>
2017-10-20 20:35:26 -0400

    paralization working

--- hmk4/cumulativeSum.c
+++ hmk4/cumulativeSum.c
@@ -5,24 +5,40 @@
 int main(){
 
 	int sequence[10] = {0,1,2,3,4,5,6,7,8,9};
+	int aux[10];
+
 	for(int k=0; k<10; k++){
 		printf("Numero: \n");
 		scanf("%d", &sequence[k]);
+		aux[k] = sequence[k];
 	}
 	int jump=0;
 	int i,j; 
-	printf("Sizeof %d", (int)(sizeof(sequence)/sizeof(int)));
 	while(pow(2,jump) < 10){
-		#pragma omp for schedule(static)  			
-		for(i=9; i>0; i--) {
-			j = i -pow(2,jump);
-			sequence[i] = sequence[i] + sequence[j];	
-			printf("%d ", sequence[i]);	
-			if((i-1-pow(2,jump))<0){
-				i=0;	
-				}	
-		
+
+		#pragma omp parallel
+		{
+			
+			#pragma omp for 
+			for(i=9; i>=0; i--) {
+
+				j = i-pow(2,jump);
+					
+				if(j>=0){
+				      aux[i] = sequence[i] + sequence[j];	
+				      printf("%d ",aux[i]);
+					}	
+			
+			}
+			#pragma omp barrier
+			
+			#pragma omp for
+			for(i=0; i<10; i++){
+				sequence[i] = aux[i];
+			}
+			#pragma omp barrier 
 		}
+		
 		printf("\n ");
 		jump++;
 		
