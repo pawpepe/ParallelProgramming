f4b9819062ad0dac75ec4eacc99487672168648a
Anna <apawlickamaule@gmail.com>
2017-10-23 14:14:26 -0400

    Parallel programming

---
+++ hmk4/solution/tree.c
@@ -0,0 +1,103 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+void init(int **sequence, int n){
+	
+}
+
+
+int main(){
+
+	clock_t start, end;
+	int *sequence;
+	double cpu_time_used;
+	int s_seq,d,i,j;		
+        int log2,last, u;
+	int aux, index1, index2,index, p;
+        //#pragma omp parallel for private(sequence)
+        for(u=1; u<=8; u*=2){
+           start = clock();
+	for(p=8; p<=14; p++){
+		s_seq =pow(2,p);
+                
+		//alocating memory for the array of numbers to be summed and it's auxiliar one 
+        	//init(&sequence, s_seq);
+ 		
+		int k;
+		if(!(sequence = (int *) malloc (sizeof (int) * s_seq))) return -1;
+		//initializing the array and its auxiliar 
+		for(k=0; k< s_seq; k++){
+			sequence[k]=k;
+		}
+		/**for(i=0; i < s_seq; i++){
+			printf("%d ", sequence[i]);
+		} 
+		printf("\n");**/
+		log2 = (int)(log(s_seq)/log(2));
+		printf("log: %d \n",log2);
+		
+                for(i=0; i<log2; i++){
+			#pragma opm parallel for privat(j) num_threads(u)
+			for(j=pow(2,i)-1; j< s_seq; j+=pow(2,i+1)){
+				index =j+pow(2,i);
+				if(index < s_seq)
+					sequence[index] += sequence[j];
+		
+			}
+			#pragma omp barrier 
+			
+                }
+
+		
+		last = sequence[s_seq-1];
+		sequence[s_seq-1] =0 ;
+		/**printf("\n");
+		for(int l=0; l < s_seq; l++)
+			    printf("%d ", sequence[l]);
+		 	
+		         printf("\n\n");**/
+
+		
+		for(i=log2; i>0 ; i--){
+			#pragma opm parallel for private(j) num_threads(u)
+			for(j=s_seq-1; j>=0;j-=pow(2,i)){
+			    index =  j - pow(2,i-1);
+			    aux = sequence[index];
+			    sequence[index] = sequence[j];
+			    sequence[j] += aux;
+		
+   			}
+			#pragma omp barrier 
+			/**for(int l=0; l < s_seq; l++)
+			    printf("%d ", sequence[l]);
+		 	
+		         printf("\n");**/
+
+		}
+		sequence[0] =last;
+		/**printf("\n\n\n\n");
+		for(i=0; i < s_seq; i++){
+			printf("%d ", sequence[i]);
+		} **/	
+		
+		
+		free(sequence);
+		
+
+      } printf("Threads: %d Time: ", u);
+	end = clock();
+	cpu_time_used = ((double)(end-start))/CLOCKS_PER_SEC;
+	
+	printf("%f\n", cpu_time_used);
+			
+}
+
+       
+return 0;
+
+}
---
+++ hmk4/solution/tree2.c
@@ -0,0 +1,80 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+void init(int **sequence, int n){
+	
+}
+
+
+int main(){
+
+	clock_t start, end;
+	int *sequence;
+	double cpu_time_used;
+	start = clock();
+	int s_seq,d,i,j;		
+        int log2;
+	int aux, index, index2,p;
+	for(p=2; p<=4; p++){
+		s_seq =pow(2,p);
+                
+		//alocating memory for the array of numbers to be summed and it's auxiliar one 
+        	//init(&sequence, s_seq);
+ 		
+		int k;
+		if(!(sequence = (int *) malloc (sizeof (int) * s_seq))) return -1;
+		//initializing the array and its auxiliar 
+		for(k=0; k< s_seq; k++){
+			sequence[k]=k;
+			}
+		log2 = (int)log(s_seq)/log(2);
+
+		for(i=0; i< log2; i++){
+	
+			for(j=0; j< s_seq; j++){
+				 if((j% (int)pow(2,i+1)) == (int) (pow(2,i+1)-1)){
+					index =j+pow(2,i);
+					sequence[j] += sequence[index];
+				}
+			}
+
+                }
+		
+		sequence[s_seq -1] =0 ;
+		
+		
+		for(i=log2; i>=0 ; i--){
+			for(j=0; j< s_seq; j++){
+			    if((j%(int)pow(2,i+1)) == (int)( pow(2,i+1)-1)){
+				index = j - pow(2,i);
+				aux = sequence[j];
+				sequence[j]  = sequence[index] + aux;
+				sequence[index] = aux;
+			}
+		
+   			}
+		}
+
+		for(i=0; i < s_seq; i++){
+			printf("%d ", sequence[i]);
+		} 	
+		printf("\n");
+		
+		//free(sequence);
+		
+
+      }
+
+       
+	end = clock();
+	cpu_time_used = ((double)(end-start))/CLOCKS_PER_SEC;
+	
+	printf("%f\n", cpu_time_used);
+	return 0;
+
+}
